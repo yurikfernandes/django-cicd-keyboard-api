041e306c2d818b44e74dacdcb180c5aad68ad156
Yuri Fernandes <yurikfernandes@gmail.com>
2024-11-10 15:44:11 -0300

    Update workflow

--- keyboards/models.py
+++ keyboards/models.py
@@ -25,7 +25,7 @@ class Keyboard(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.name
+        return f"{self.name} - {self.category}"
     
 
 class KeyboardFeature(models.Model):
