cefa799a3bbd7a39eb30f258b3e844dc73b2126c
Yuri Fernandes <yurikfernandes@gmail.com>
2024-11-15 17:18:43 -0300

    Add views and templates

--- keyboards/views.py
+++ keyboards/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+from keyboards.models import Keyboard, KEYBOARDS_CATEGORIES
+
+def homepage(request):
+    keyboards = Keyboard.objects.all().order_by('-overall_rating')[:5]
+    keyboards_dict = []
+    for keyboard in keyboards:
+        keyboards_dict.append(keyboard.get_keyboard_dict())
+
+    return render(
+        request, 'homepage.html', {'keyboards': keyboards_dict}
+    )
+
+def keyboard_list(request):
+    keyboards = Keyboard.objects.all()
+    return render(
+        request, 'keyboard_list.html', {'keyboards': keyboards}
+    )
--- music_keyboards_api/urls.py
+++ music_keyboards_api/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from keyboards.views import homepage, keyboard_list
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', homepage, name='homepage'),
+    path('teclados/', keyboard_list, name='keyboard_list'),
 ]
